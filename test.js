050a1593a7d5aae8e787d26788562dfff890d089
MD MASUM ALI <khanmasum@MDs-Laptop.local>
2024-09-24 20:00:14 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'masum' ,
+    age: 25,
+    study:{
+        primary: 'tajkera',
+        high:{
+            firstHigh:'mohorali',
+            secondHigh: 'jahangir'
+        }
+    }
+};
+
+console.log(person.study.high.third);
